c2c23354f0f232cc6dcfc447ddb0c178a58dd7f4
Alexander <bulgakov453@gmail.com>
2023-06-18 16:27:04 +0500

    Task2,3 completed

--- SimpleNumbers.java
+++ SimpleNumbers.java
@@ -2,14 +2,14 @@ public class SimpleNumbers {
         public static void main(String[] args) {
             int sum = 0;
             for (int i = 2; i <= 1000; i++) {
-                if (isPrime(i)) {
+                if (isSimple(i)) {
                     sum += i;
                 }
             }
             System.out.println("Сумма простых чисел от 1 до 1000 равна " + sum);
         }
 
-        private static boolean isPrime(int number) {
+        private static boolean isSimple(int number) {
             if (number == 1) {
                 return false;
             }
