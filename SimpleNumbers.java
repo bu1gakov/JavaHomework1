ece9f5f46ae916b40d40e76dce4f846aa34dd7d4
Alexander <bulgakov453@gmail.com>
2023-06-18 16:26:29 +0500

    Task2,3 completed

--- SimpleNumbers.java
+++ SimpleNumbers.java
@@ -1,17 +1,23 @@
 public class SimpleNumbers {
-    public static void main (String [] agrs) {
-        int sum = 0;
-        for (int i = 1; i <= 1000; i++) {
-            if (isSimple(i)) {
-                sum += i;
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 2; i <= 1000; i++) {
+                if (isPrime(i)) {
+                    sum += i;
+                }
             }
+            System.out.println("Сумма простых чисел от 1 до 1000 равна " + sum);
         }
-        System.out.println("Сумма простых чисел от 1 до 1000: " +sum);
-    }
-    private static boolean isSimple(int n) {
-        for (int i = 1; i<= Math.sqrt(n); i++) {
-            return false;
+
+        private static boolean isPrime(int number) {
+            if (number == 1) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
-}
